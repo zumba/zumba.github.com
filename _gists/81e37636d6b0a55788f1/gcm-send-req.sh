8d84de1324e14755418d59a60c30ac00acec79a2
Chris Saylor <cjsaylor@gmail.com>
2022-02-23 21:31:02 -0500

    Modernized website and shifting focus to hiring.

--- _gists/81e37636d6b0a55788f1/gcm-send-req.sh
+++
@@ -1,4 +0,0 @@
-curl -X "POST" "https://android.googleapis.com/gcm/send" \
-	-H "Authorization: key=[INSERT_PRIVATE_SERVER_KEY_HERE]" \
-	-H "Content-Type: application/json" \
-	-d "{\"registration_ids\":[\"USERS_SUBSCRIPTION_ID\"],\"data\":{\"foo\":\"bar\"}}"
--- _gists/9acaa7d1504e57ece36e/example_trigger.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-public function addItem(Item $item, $quantity) {
-	// Trigger event before the item is added
-	EventManager::trigger('cart.before-add', compact('item', 'quantity'));
-	// Some cart logic here to add the item
-	// Trigger event after the item is added
-	EventManager::trigger('cart.after-add', compact('item', 'quantity'));
-}
